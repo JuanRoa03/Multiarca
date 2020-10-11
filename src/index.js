3fd1d29d7327bd7a91c83dee8037cce39b430a22
JuanDaCalderon <71572762+JuanDaCalderon@users.noreply.github.com>
2020-10-10 20:23:55 -0500

    Index.js 10/10/2020
    Actualizacion Index

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import './Assets/css/index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import './includes/bootstrap'
+import './includes/bootstrap';
+import { FirebaseAppProvider } from 'reactfire';
+import firebaseConfig from './utils/firebase';
+import { BrowserRouter } from "react-router-dom";
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  <FirebaseAppProvider firebaseConfig ={firebaseConfig}>
+    <Suspense fallback={'Conectando con firebase'}>
+      <BrowserRouter>
+        <App/>
+      </BrowserRouter>
+    </Suspense>
+  </FirebaseAppProvider>
+ ,document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
